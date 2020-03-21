3840e2fdcc09b5bd33eebd979c261f6429b260eb
Joey Schrader <jschrader@overstock.com>
2020-03-21 08:02:45 -0600

    MongoDB connected

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 require('dotenv').config();
-
-const express = require('express');
-
 const PORT = process.env.PORT || 5000;
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 
 /** controllers */
 const auth = require('./controllers/auth');
 
 const app = express();
 
+const client = new MongoClient(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+})
+
 /** routes */
 app.use('/auth', auth);
 
 app.listen(PORT, () => {
-    console.log('SERVER RUNNING ON PORT')
-})
+    console.log('SERVER RUNNING ON PORT');
+    client.connect(err => {
+        console.log('successfully connected to database')
+        const database = client.db('sample_weatherdata')
+        const collection = database.collection('data')
+        console.log(collection)
+    });
+});
