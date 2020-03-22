eea8bd00175fa7af9f55e85910607527fa1bb414
Joey Schrader <jschrader@overstock.com>
2020-03-22 09:24:30 -0600

    adjust server.js imports, delete database.js

--- server.js
+++ server.js
@@ -2,26 +2,25 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const express = require('express');
 const mongoose = require('mongoose');
+const app = express();
 
-/** ESTABLISH DATABASE CONNECTION */
-const dbUri = process.env.MONGODB_URL || 'mongodb://localhost/weather_data';
-const options = {
+/** DATABASE CONFIG */
+const mongoAtlasUri = process.env.MONGODB_URL || 'mongodb://localhost/weather_data';
+const configOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     dbName: 'sample_weatherdata'
 };
 
-const app = express();
-
 /** controllers */
 const auth = require('./controllers/auth');
-const weather = require('./controllers/weather')
+const weather = require('./controllers/weather');
 
 /** routes */
 app.use('/auth', auth);
 app.use('/weather', weather);
 
-mongoose.connect(dbUri, options)
+mongoose.connect(mongoAtlasUri, configOptions)
     .then(() => {
         console.log('Database connection established!');
 
