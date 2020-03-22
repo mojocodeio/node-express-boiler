71426cc04133254dc4a51cbcc49a14de0ecd2156
Joey Schrader <jschrader@overstock.com>
2020-03-22 14:15:03 -0600

    proxy server set up in client cra

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 
 /** DATABASE CONFIG */
@@ -16,9 +17,17 @@ const configOptions = {
 const auth = require('./controllers/auth');
 const weather = require('./controllers/weather');
 
+/** middleware */
+app.use(express.static(path.resolve(__dirname, './client/build')))
+
 /** routes */
 app.use('/auth', auth);
 app.use('/weather', weather);
+
+app.get('*', (req, res) => {
+    console.log('req made!!')
+    res.sendFile(path.resolve(__dirname, '..client/build', 'index.html'))
+});
 
 mongoose.connect(mongoAtlasUri, configOptions)
     .then(() => {
