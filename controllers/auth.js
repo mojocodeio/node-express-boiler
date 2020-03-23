0cdd2c14e641e0c7ba5c359d53f67d7ae4cb8d89
Joey Schrader <jschrader@overstock.com>
2020-03-23 16:10:06 -0600

    added express session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    console.log('Request session:', req.session.cookie)
+    res.send('hello auth route')
+})
+
 router.post('/register', (req, res) => {
     res.send('Hello, I am the register route')
 });
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const session = require('express-session')
 const app = express();
 
 /** DATABASE CONFIG */
@@ -25,6 +26,16 @@ const weather = require('./controllers/weather');
 
 /** middleware */
 app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'secret',
+  name: 'sid',
+  saveUninitialized: false,
+  resave: false,
+  cookie: {
+    maxAge: 1000*60*60*2,
+    sameSite: true,
+  }
+}))
 
 /** routes */
 app.use('/auth', auth);
