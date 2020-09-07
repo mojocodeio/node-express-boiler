5f8d5fa96e6df57921072696d380ac3a51f7911b
Joey Schrader <jschrader@overstock.com>
2020-09-06 17:35:12 -0700

    styles for form

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -8,7 +8,6 @@ import Auth from './Auth/Auth';
 import AuthRoute from './Auth/AuthRoute/AuthRoute';
 import Dashboard from './Dashboard/Dashboard';
 import User from './Auth/User/User';
-import FlashError from './FlashError/FlashError';
 
 /** styles */
 import styles from './App.module.css';
@@ -17,13 +16,19 @@ export const App = () => {
 	return (
 		<div className={styles.app}>
 			<User>
-				<Navbar />
-				<FlashError />
-				<Switch>
-					<Route exact path={'/'} component={Home} />
-					<Route path={'/login'} component={Auth} />
-					<AuthRoute path={'/dashboard'} component={Dashboard} />
-				</Switch>
+				<div className={styles.header}>
+					<Navbar />
+				</div>
+				<div className={styles.content}>
+					<Switch>
+						<Route exact path={'/'} component={Home} />
+						<Route path={'/login'} component={Auth} />
+						<AuthRoute path={'/dashboard'} component={Dashboard} />
+					</Switch>
+				</div>
+				<div className={styles.footer}>
+					Footer
+				</div>
 			</User>
 		</div>
 	);
--- client/src/App/Auth/Auth.js
+++ client/src/App/Auth/Auth.js
@@ -23,6 +23,7 @@ export const Auth = ({
     const [password, setPassword] = useState('');
     const [alreadyMember, setAlreadyMember] = useState(false);
     const url = alreadyMember ? loginUrl : registerUrl;
+    const title = alreadyMember ? 'Login' : 'Register';
 
     if (userId) {
         return (
@@ -35,28 +36,40 @@ export const Auth = ({
             className={styles.auth}
             onSubmit={e => e.preventDefault()}
         >
+            <h3 className={styles.title}>{title}</h3>
             <input
+                className={`${styles['input-section']} ${styles.username}`}
                 value={userName}
                 onChange={e => setUserName(e.target.value)}
+                placeholder='User Name'
             />
             <input
+                className={`${styles['input-section']} ${styles.password}`}
                 value={password}
                 onChange={e => setPassword(e.target.value)}
+                placeholder='Password'
             />
-            <input
-                type="checkbox"
-                checked={alreadyMember}
-                onChange={() => setAlreadyMember(!alreadyMember)}
-            />
-            <label>Already a member?</label>
-            <button
-                className={styles['auth-button']}
-                onClick={() => {
-                    handleAuthenticateUser({ userName, password }, url)
-                    setUserName('')
-                    setPassword('')
-                }
-            }>Submit</button>
+            <div className={`${styles['input-section']} ${styles['checkbox-container']}`}>
+                <input
+                    className={styles.checkbox}
+                    type="checkbox"
+                    checked={alreadyMember}
+                    onChange={() => setAlreadyMember(!alreadyMember)}
+                />
+                <label>Already a member?</label>
+            </div>
+            <div className={styles['input-section']}>
+                <button
+                    className={styles['auth-button']}
+                    onClick={() => {
+                        handleAuthenticateUser({ userName, password }, url)
+                        setUserName('')
+                        setPassword('')
+                    }
+                }>
+                    Submit
+                </button>
+            </div>
         </form>
     );
 };
